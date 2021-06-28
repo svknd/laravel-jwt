d7e675b76fa06f9fd2318744faa15485e83a034c
Irvan Maulana <me@tukangremot.com>
2021-06-28 14:48:41 +0700

    add: Jwt

---
+++ src/Models/Jwt.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Svknd\Laravel\Jwt\Models;
+
+use Firebase\JWT\JWT as JwtBase;
+
+
+trait Jwt
+{
+    public function generateToken()
+    {
+        $issuedAt = time();
+        // $expirationTime = $issuedAt + config('auth.token_expiration');
+
+        $accessToken = JwtBase::encode(array(
+            'identifier' => $this->id,
+            'type' => 'user',
+            // "iss" => "http://example.org",
+            // "aud" => "http://example.com",
+            'iat' => $issuedAt,
+            // 'exp' => $expirationTime
+        ), config('auth.access_token_key'));
+
+        $refreshToken = JwtBase::encode(array(
+            'identifier' => $this->id,
+            'iat' => $issuedAt,
+        ), config('auth.refresh_token_key'));
+
+        return $accessToken;
+    }
+}
